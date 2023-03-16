93fec31f2b23098ce641825131e34eeecd29ec37
Oliver Michels <oliver.michels@gmail.com>
2023-03-16 22:03:18 +0100

    no explicit iterator

--- main.tf
+++ main.tf
@@ -11,13 +11,19 @@ resource "azurerm_container_registry" "acr" {
 
   dynamic "georeplications" {
 
-    for_each = var.georeplications == null && var.sku == "Premium" ? [] : ["georeplications"]
+    #    for_each = var.georeplications == null && var.sku == "Premium" ? [] : ["georeplications"]
+    #    content {
+    #      location = georeplications.value["location"]
+    #      #zone_redundancy_enabled   = each.value.georeplications.zone_redundancy_enabled
+    #      #regional_endpoint_enabled = each.value.georeplications.regional_endpoint_enabled
+    #      #tags                      = each.value.georeplications.tags
+    #    }
+
+    for_each = var.georeplications
     content {
       location = georeplications.value["location"]
-      #zone_redundancy_enabled   = each.value.georeplications.zone_redundancy_enabled
-      #regional_endpoint_enabled = each.value.georeplications.regional_endpoint_enabled
-      #tags                      = each.value.georeplications.tags
     }
+
   }
 
   tags = var.tags
