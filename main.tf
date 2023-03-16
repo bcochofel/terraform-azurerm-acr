2da0ad07b9636efc34346efac5cfef9f803e3448
Oliver Michels <oliver.michels@gmail.com>
2023-03-16 22:40:44 +0100

    iterating correct

--- main.tf
+++ main.tf
@@ -11,17 +11,13 @@ resource "azurerm_container_registry" "acr" {
 
   dynamic "georeplications" {
 
-    #    for_each = var.georeplications == null && var.sku == "Premium" ? [] : ["georeplications"]
-    #    content {
-    #      location = georeplications.value["location"]
-    #      #zone_redundancy_enabled   = each.value.georeplications.zone_redundancy_enabled
-    #      #regional_endpoint_enabled = each.value.georeplications.regional_endpoint_enabled
-    #      #tags                      = each.value.georeplications.tags
-    #    }
-
     for_each = (var.georeplications != null && var.sku == "Premium") ? var.georeplications : []
     content {
-      location = georeplications.value["location"]
+      location                  = georeplications.value["location"]
+      zone_redundancy_enabled   = georeplications.value["zone_redundancy_enabled"]
+      regional_endpoint_enabled = georeplications.value["regional_endpoint_enabled"]
+      tags                      = georeplications.value["tags"]
+
     }
 
   }
